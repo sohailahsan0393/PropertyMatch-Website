d05671248ccc84b201f418ddf154b359af28bd9a
Sohail Ahsan <l1f21bsse0393@ucp.edu.pk>
2026-04-03 02:25:03 +0500

    PropertyMatch Full Project

--- PropertyMAtch-Website/resources/views/components/nav-link.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['href', 'active' => false])
-
-<a href="{{ $href }}"
-   class="nav-link {{ $active ? 'text-white rounded' : 'text-black' }}"
-   style="{{ $active ? 'background-color: #5038ED;' : '' }}"
-   aria-current="{{ $active ? 'page' : 'false' }}">
-    {{ $slot }}
-</a>
-
